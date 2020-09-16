e2f6676782d844111b573dff726a7a0d39fa3cbd
Martin lee <e854008715@qq.com>
2020-09-16 20:16:36 +0800

    changed pages

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
-<?php get_header(); ?>
-    <!-- Start of main-content -->
-    <section id="content" class="site-section">
-        <div class="container">
-            <div class="row">
-                <div class="post-title">
-                    <h3><?php _e( 'Posts', 'highstarter'); ?></h3>
-                </div>
-            </div>
-            <div class="row blog-entries">
-                <div class="main-content">
-                    <div class="row">
-                        <?php
-                    //Dynamic content here
-                    if (have_posts() ) :
-                        while (have_posts() ) : the_post();
-                        	get_template_part( 'template-parts/post/content', 'excerpt', get_post_format() );
-                        endwhile;
-                    highstarter_numeric_posts_nav();
-                    else :
-                        _e( 'There are no posts!', 'highstarter');                
-                    endif;
-                    ?>
-                    </div>
-                </div>
-                <!-- END of main-content -->
-                <!-- Show Sidebar -->
-                <?php get_sidebar() ?>
-            </div>
-        </div>
-    </section>
-<?php get_footer(); ?>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
